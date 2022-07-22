3e477642565fe2fad38903747a6f45497bf5e96c
Vicente Viera <plantedcoot706@gmail.com>
2022-07-21 19:53:46 -0500

    Nav Bar redesing, Links added and animations for the buttons

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,23 +1,34 @@
 import React from 'react'
-import Button from './Button'
+import {Link} from 'react-router-dom'
 
 const NavBar = () => {
     return (
         <div className="bg-gradient-to-r from-transparent via-white w-full h-[5.25rem] brightness-125 pb-0">
             <div className="w-full h-20 bg-black">
                 <nav className="flex justify-around h-full">
-                    <div className="flex h-full w-1/3 flex-1 justify-around">
-                        <Button/>
-                        <Button/>
+                    <div className="flex h-full w-1/3 justify-around">
+                        <Link
+                            to="/data_structures" 
+                            className="transition ease-in-out duration-300 delay-75 text-white text-xl brightness-125 self-center hover:-translate-y-1 hover:brightness-200 hover:scale-105"
+                            >Data Structures
+                        </Link>
+                        <Link
+                            to="/algorithms"
+                            className="transition ease-in-out duration-300 delay-75 text-white text-xl brightness-125 self-center hover:-translate-y-1 hover:brightness-200 hover:scale-105"
+                            >Algorithms
+                        </Link>
                     </div>
+                    
+                    <Link
+                        to="/"
+                        className="text-white text-3xl brightness-125 self-center"
+                        >DataSAV
+                    </Link>
 
-                    <div className="self-center w-1/3 flex-1">
-                        <p className="text-white text-3xl brightness-125">DataSAV</p>
-                    </div>
-
-                    <div className="bg-white h-2/3 w-1/3 flex-1 self-center">
-
-                    </div>                 
+                    <div className="flex gap-3 justify-end w-1/3 self-center">
+                        <p className="text-white">mis redes</p>
+                        <p className="text-white">mis redes</p>
+                    </div>             
                 </nav>
             </div>
         </div>
